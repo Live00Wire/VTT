5f8df6c838bd431f11d256ddcb6958911c3a12b1
Live00Wire <40453899+Live00Wire@users.noreply.github.com>
2026-04-06 11:34:40 +1000

    updated

--- server.js
+++ server.js
@@ -185,5 +185,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => { console.log(`🚀 Server running on http://localhost:${PORT}`); });
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => { 
+    console.log(`🚀 Server running on port ${PORT}`); 
+});
